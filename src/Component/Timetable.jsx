39e431dff5f91862b45b9bb5c991ac205a0e6a48
Akshaikhanna <90137770+Akshaikhanna@users.noreply.github.com>
2023-01-20 22:01:34 +0530

    first commit

---
+++ src/Component/Timetable.jsx
@@ -0,0 +1,103 @@
+import React from 'react';
+import './Timetable.css';
+
+function Timetable() {
+  return (
+    <>
+    <div className='day'>
+    <div className='d'>Day/Period Time</div>
+        <div className='one'>1</div>
+        <div className='two'>2</div>
+        <div className='br'>B</div>
+        <div className='three'>3</div>
+        <div className='four'>4</div>
+        <div className='br'>L</div>
+        <div className='five'>5</div>
+        <div className='six'>6</div>
+        <div className='br'>B</div>
+        <div className='seven'>7</div>
+        <div className='eigth'>8</div>
+      {/* <input type="text" className="table" /> */}
+    </div>
+    <div className='day'>
+    <div className='d'>Monday</div>
+        <div className='one'><input type="text" className='input' ></input></div>
+        
+        <div className='two'><input type="text" className='input' ></input></div>
+        <div className='br'>r</div>
+        <div className='three'><input type="text" className='input' ></input></div>
+        <div className='four'><input type="text" className='input' ></input></div>
+        <div className='br'>u</div>
+        <div className='five'><input type="text" className='input' ></input></div>
+        <div className='six'><input type="text" className='input' ></input></div>
+        <div className='br'>r</div>
+        <div className='seven'><input type="text" className='input' ></input></div>
+        <div className='eigth'><input type="text" className='input' ></input></div>
+      {/* <input type="text" className="table" /> */}
+    </div>
+    <div className='day'>
+    <div className='d'>Tuesday</div>
+        <div className='one'><input type="text" className='input' ></input></div>
+        <div className='two'><input type="text" className='input' ></input></div>
+        <div className='br'>e</div>
+        <div className='three'><input type="text" className='input' ></input></div>
+        <div className='four'><input type="text" className='input' ></input></div>
+        <div className='br'>n</div>
+        <div className='five'><input type="text" className='input' ></input></div>
+        <div className='six'><input type="text" className='input' ></input></div>
+        <div className='br'>e</div>
+        <div className='seven'><input type="text" className='input' ></input></div>
+        <div className='eigth'><input type="text" className='input' ></input></div>
+      {/* <input type="text" className="table" /> */}
+    </div>
+    <div className='day'>
+    <div className='d'>Wednesday</div>
+        <div className='one'><input type="text" className='input' ></input></div>
+        <div className='two'><input type="text" className='input' ></input></div>
+        <div className='br'>a</div>
+        <div className='three'><input type="text" className='input' ></input></div>
+        <div className='four'><input type="text" className='input' ></input></div>
+        <div className='br'>c</div>
+        <div className='five'><input type="text" className='input' ></input></div>
+        <div className='six'><input type="text" className='input' ></input></div>
+        <div className='br'>a</div>
+        <div className='seven'><input type="text" className='input' ></input></div>
+        <div className='eigth'><input type="text" className='input' ></input></div>
+      {/* <input type="text" className="table" /> */}
+    </div>
+    <div className='day'>
+    <div className='d'>Thursday</div>
+        <div className='one'><input type="text" className='input' ></input></div>
+        <div className='two'><input type="text" className='input' ></input></div>
+        <div className='br'>k</div>
+        <div className='three'><input type="text" className='input' ></input></div>
+        <div className='four'><input type="text" className='input' ></input></div>
+        <div className='br'>h</div>
+        <div className='five'><input type="text" className='input' ></input></div>
+        <div className='six'><input type="text" className='input' ></input></div>
+        <div className='br'>k</div>
+        <div className='seven'><input type="text" className='input' ></input></div>
+        <div className='eigth'><input type="text" className='input' ></input></div>
+      {/* <input type="text" className="table" /> */}
+    </div>
+    <div className='day'>
+    <div className='d'>Friday</div>
+        <div className='one'><input type="text" className='input' ></input></div>
+        <div className='two'><input type="text" className='input' ></input></div>
+        <div className='br'></div>
+        <div className='three'><input type="text" className='input' ></input></div>
+        <div className='four'><input type="text" className='input' ></input></div>
+        <div className='br'></div>
+        <div className='five'><input type="text" className='input' ></input></div>
+        <div className='six'><input type="text" className='input' ></input></div>
+        <div className='br'></div>
+        <div className='seven'><input type="text" className='input' ></input></div>
+        <div className='eigth'><input type="text" className='input' ></input></div>
+      {/* <input type="text" className="table" /> */}
+    </div>
+    </>
+   
+  )
+}
+
+export default Timetable;
